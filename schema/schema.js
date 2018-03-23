b13997eb740e1ba4e2f4c0ac26d9d9207605b8b3
astadipati <astadipati@outlook.com>
2018-03-24 00:07:35 +0700

    Hardcode schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,50 @@
 // ini centernya data diolah dimari
 const graphql = require ('graphql');
+const _= require ('lodash');
 
 const {
-    GraphQLObjectType
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema //library inti dari schema yang ditaruh di root
 } = graphql;
+
+// manual data
+const users = [
+    {id: '23', firstName: 'Rama', age: 22},
+    {id: '24', firstName: 'Astadipati', age: 23},
+    {id: '25', firstName: 'Astadinata', age: 24},
+    {id: '26', firstName: 'Kenshin', age: 25},
+    {id: '27', firstName: 'Kinarra', age: 22}
+];
+
+const UserType = new GraphQLObjectType({
+    // bagian ini ada 2 property
+    // property
+    name: 'User',
+    // objek property
+    fields: {
+        id:  {type: GraphQLString} ,
+        firstName: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    // root ini berdasarkan id
+    // beri saya id saya akan kembalikan ke UserType
+    name : 'RootQueryType',
+    fields:{
+        user: {
+            type: UserType,
+            args:{id:{type:GraphQLString}},
+            resolve(parentValue, args){ //actual data
+                return _.find(users,{id: args.id});
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require ('express');
 const expressGraphQL = require('express-graphql'); //handle graphql
 
+const schema = require('./schema/schema'); //import schema
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
+    schema, //panggil schema
     graphiql: true
 }));
 
